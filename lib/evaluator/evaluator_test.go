33500b729db6a6d919a997784dbc03269a57876e
--get <[tb0moore@hotmail.com]>
2018-10-12 04:46:21 -0600

    Eval testing for bools.

--- lib/evaluator/evaluator_test.go
+++ lib/evaluator/evaluator_test.go
@@ -63,3 +63,32 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	return true
 }
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+
+		evaluated := testEval(tt.input)
+
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+
+	result, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Errorf("Object is not a Boolean. got=%t, want=%t", result.Value, expected)
+		return false
+	}
+
+	return true
+}
